f6eab03f4cc3403126f652820ba94e68bfb06ba6
György Frivolt <g.frivolt@netronixgroup.com>
2016-11-23 19:47:01 +0100

    updated host name

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :echo_bot, EchoBot.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "koinbot.herokuapp.com", port: 443],
+  url: [scheme: "https", host: "elixir-bot.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
